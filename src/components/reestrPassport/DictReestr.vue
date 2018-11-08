fc193ac38587fb44c6c1ffbeefc68d1a20600d65
Semin Dmitry <semin@imp-m.ru>
2018-11-08 15:48:13 +0300

    Пуш

--- src/components/reestrPassport/DictReestr.vue
+++ src/components/reestrPassport/DictReestr.vue
@@ -10,7 +10,7 @@
 				</MenuGroup>
 			</Menu>
 		</Sider>
-	
+
 		<Content>
 			<Card>
 				<dict-area v-if="showDictArea"></dict-area>
@@ -23,47 +23,30 @@
 </template>
 
 <script>
-import * as funcUtils from "../../assets/js/utils/funcUtils";
-import DictArea from "../dictonary/DictArea";
-import DictCategory from "../dictonary/DictCategory";
-import DictSource from "../dictonary/DictSource";
-import DictReason from "../dictonary/DictReason";
-import TablePoptip from "./../verstka/TablePoptip.vue";
+  import * as funcUtils from "../../assets/js/utils/funcUtils";
+  import DictArea from "../dictonary/DictArea";
+  import DictCategory from "../dictonary/DictCategory";
+  import DictSource from "../dictonary/DictSource";
+  import DictReason from "../dictonary/DictReason";
+  import TablePoptip from './../verstka/TablePoptip.vue';
 
-export default {
-	name: "DictReestr",
-	components: {
-		DictArea,
-		DictCategory,
-		DictSource,
-		DictReason,
-		TablePoptip
-	},
-	mounted() {
-		let vm = this;
-		let dictReestr = funcUtils.getfromLocalStorage("dictReestr");
+  export default {
+    name: "DictReestr",
+    components: {
+      DictArea,
+      DictCategory,
+      DictSource,
+      DictReason,
+      TablePoptip
+    },
+    mounted() {
+      let vm = this;
+      let dictReestr = funcUtils.getfromLocalStorage('dictReestr');
 
-		let dictArea = this.$store.state.dictArea.bean;
-		let dictSource = this.$store.state.dictSource.bean;
-		let dictCategory = this.$store.state.dictCategory.bean;
-		let dictReason = this.$store.state.dictReason.bean;
-
-		this.dictionaries.push({
-			beanName: dictArea,
-			description: "Типы территорий"
-		});
-		this.dictionaries.push({
-			beanName: dictSource,
-			description: "Источники данных"
-		});
-		this.dictionaries.push({
-			beanName: dictCategory,
-			description: "Категории ТС"
-		});
-		this.dictionaries.push({
-			beanName: dictReason,
-			description: "Основания включения"
-		});
+      let dictArea = this.$store.state.dictArea.bean;
+      let dictSource = this.$store.state.dictSource.bean;
+      let dictCategory = this.$store.state.dictCategory.bean;
+      let dictReason = this.$store.state.dictReason.bean;
 
 		(async () => {
 			try {
@@ -87,154 +70,149 @@ export default {
 						moduleName: vm.$store.state.dictReason.moduleName
 					};
 
-					eventResponse = await vm.$store.dispatch("prepareData", {
-						beanName: dictCategory,
-						method: null,
-						params: null,
-						cid: null
-					});
-					if (eventResponse.status === 200) {
-						let data = eventResponse.response;
-						if (data.length > 0) {
-							let dataJson = JSON.parse(data);
-							let respData = dataJson.data;
-							let respError = dataJson.error;
-							if (!funcUtils.isNull(respData)) {
-								if (dataJson.method === "addCID") {
-									dictReestr[dictCategory].cid = respData.cid;
-									funcUtils.addToLocalStorage("dictReestr", dictReestr);
-									vm.$store.dispatch("dictCategorySetCid", respData.cid);
-									eventResponse = await vm.$store.dispatch("prepareData", {
-										method: "getData",
-										cid: respData.cid
-									});
-									await vm.$store.dispatch("fillModule", {
-										event: eventResponse
-									});
-									vm.activeDict = dictCategory;
-								}
-							} else {
-								if (!funcUtils.isNull(respError)) {
-									alert(respError.errorMsg);
-								}
-							}
-						}
-					}
-				} else {
-					for (let prop in dictReestr) {
-						if (dictReestr.hasOwnProperty(prop)) {
-							if (funcUtils.isNotEmpty(dictReestr[prop].cid)) {
-								vm.activeDict = prop;
-							}
-						}
-					}
-				}
-			} catch (e) {
-				alert(e.message);
-			}
-		})();
-	},
-	destroyed() {
-		this.$store.dispatch("dictCategorySetCid", null);
-		this.$store.dispatch("dictCategorySetData", {
-			data: null
-		});
+            eventResponse = await vm.$store.dispatch('prepareData', {
+              beanName: dictCategory,
+              method: null,
+              params: null,
+              cid: null
+            });
+            if (eventResponse.status === 200) {
+              let data = eventResponse.response;
+              if (data.length > 0) {
+                let dataJson = JSON.parse(data);
+                let respData = dataJson.data;
+                let respError = dataJson.error;
+                if (!funcUtils.isNull(respData)) {
+                  if (dataJson.method === 'addCID') {
+                    dictReestr[dictCategory].cid = respData.cid;
+                    funcUtils.addToLocalStorage('dictReestr', dictReestr);
+                    vm.$store.dispatch('dictCategorySetCid', respData.cid);
+                    eventResponse = await vm.$store.dispatch('prepareData', {
+                      method: 'getData',
+                      cid: respData.cid
+                    });
+                    await vm.$store.dispatch('fillModule', {'event': eventResponse});
+                    vm.activeDict = dictCategory;
+                  }
+                } else {
+                  if (!funcUtils.isNull(respError)) {
+                    alert(respError.errorMsg);
+                  }
+                }
+              }
+            }
+          } else {
+            for (let prop in dictReestr) {
+              if (dictReestr.hasOwnProperty(prop)) {
+                if (funcUtils.isNotEmpty(dictReestr[prop].cid)) {
+                  vm.activeDict = prop;
+                }
+              }
+            }
+          }
+        } catch (e) {
+          alert(e.message);
+        }
+      })();
+    },
+    destroyed() {
+      this.$store.dispatch('dictCategorySetCid', null);
+      this.$store.dispatch('dictCategorySetData', {
+        data: null
+      });
 
-		this.$store.dispatch("dictSourceSetCid", null);
-		this.$store.dispatch("dictSourceSetData", {
-			data: null
-		});
+      this.$store.dispatch('dictSourceSetCid', null);
+      this.$store.dispatch('dictSourceSetData', {
+        data: null
+      });
 
-		this.$store.dispatch("dictReasonSetCid", null);
-		this.$store.dispatch("dictReasonSetData", {
-			data: null
-		});
+      this.$store.dispatch('dictReasonSetCid', null);
+      this.$store.dispatch('dictReasonSetData', {
+        data: null
+      });
 
-		this.$store.dispatch("dictAreaSetCid", null);
-		this.$store.dispatch("dictAreaSetData", {
-			data: null
-		});
-	},
-	data() {
-		return {
-			activeDict: null,
-			dictionaries: []
-		};
-	},
-	computed: {
-		showDictCategory() {
-			return this.$store.state.dictCategory.bean === this.activeDict;
-		},
-		showDictReason() {
-			return this.$store.state.dictReason.bean === this.activeDict;
-		},
-		showDictSource() {
-			return this.$store.state.dictSource.bean === this.activeDict;
-		},
-		showDictArea() {
-			return this.$store.state.dictArea.bean === this.activeDict;
-		}
-	},
-	methods: {
-		onMenuClick(dictName) {
-			let vm = this;
-			let dictReestr = funcUtils.getfromLocalStorage("dictReestr");
-			let cid = dictReestr[this.activeDict].cid;
-			let moduleName = dictReestr[this.activeDict].moduleName;
+      this.$store.dispatch('dictAreaSetCid', null);
+      this.$store.dispatch('dictAreaSetData', {
+        data: null
+      });
+    },
+    data() {
+      return {
+        activeDict: "DictCategoryView"
+      }
+    },
+    computed: {
+      showDictCategory() {
+        return this.$store.state.dictCategory.bean === this.activeDict;
+      },
+      showDictReason() {
+        return this.$store.state.dictReason.bean === this.activeDict;
+      },
+      showDictSource() {
+        return this.$store.state.dictSource.bean === this.activeDict;
+      },
+      showDictArea() {
+        return this.$store.state.dictArea.bean === this.activeDict;
+      },
+    },
+    methods: {
+      onMenuClick(dictName) {
+        let vm = this;
+        let dictReestr = funcUtils.getfromLocalStorage('dictReestr');
+        let cid = dictReestr[this.activeDict].cid;
+        let moduleName = dictReestr[this.activeDict].moduleName;
 
-			(async () => {
-				try {
-					vm.$store.dispatch("removeComponent", {
-						cid: cid
-					});
+        (async () => {
+          try {
+            vm.$store.dispatch('removeComponent', {
+              cid: cid
+            });
 
-					vm.$store.dispatch(moduleName + "SetCid", null);
-					vm.$store.dispatch(moduleName + "SetData", {
-						data: null
-					});
-					dictReestr[vm.activeDict].cid = null;
-					moduleName = dictReestr[dictName].moduleName;
+            vm.$store.dispatch(moduleName + 'SetCid', null);
+            vm.$store.dispatch(moduleName + 'SetData', {
+              data: null
+            });
+            dictReestr[vm.activeDict].cid = null;
+            moduleName = dictReestr[dictName].moduleName;
 
-					let eventResponse = await vm.$store.dispatch("prepareData", {
-						beanName: dictName,
-						method: null,
-						params: null,
-						cid: null
-					});
-					if (eventResponse.status === 200) {
-						let data = eventResponse.response;
-						if (data.length > 0) {
-							let dataJson = JSON.parse(data);
-							let respData = dataJson.data;
-							let respError = dataJson.error;
-							if (!funcUtils.isNull(respData)) {
-								if (dataJson.method === "addCID") {
-									dictReestr[dictName].cid = respData.cid;
-									funcUtils.addToLocalStorage("dictReestr", dictReestr);
-									vm.$store.dispatch(moduleName + "SetCid", respData.cid);
-									eventResponse = await vm.$store.dispatch("prepareData", {
-										method: "getData",
-										cid: respData.cid
-									});
-									await vm.$store.dispatch("fillModule", {
-										event: eventResponse
-									});
-									vm.activeDict = dictName;
-								}
-							} else {
-								if (!funcUtils.isNull(respError)) {
-									alert(respError.errorMsg);
-								}
-							}
-						}
-					}
-				} catch (e) {
-					alert(e.message);
-				}
-			})();
-		}
-	}
-};
+            let eventResponse = await vm.$store.dispatch('prepareData', {
+              beanName: dictName,
+              method: null,
+              params: null,
+              cid: null
+            });
+            if (eventResponse.status === 200) {
+              let data = eventResponse.response;
+              if (data.length > 0) {
+                let dataJson = JSON.parse(data);
+                let respData = dataJson.data;
+                let respError = dataJson.error;
+                if (!funcUtils.isNull(respData)) {
+                  if (dataJson.method === 'addCID') {
+                    dictReestr[dictName].cid = respData.cid;
+                    funcUtils.addToLocalStorage('dictReestr', dictReestr);
+                    vm.$store.dispatch(moduleName + 'SetCid', respData.cid);
+                    eventResponse = await vm.$store.dispatch('prepareData', {
+                      method: 'getData',
+                      cid: respData.cid
+                    });
+                    await vm.$store.dispatch('fillModule', {'event': eventResponse});
+                    vm.activeDict = dictName;
+                  }
+                } else {
+                  if (!funcUtils.isNull(respError)) {
+                    alert(respError.errorMsg);
+                  }
+                }
+              }
+            }
+          } catch (e) {
+            alert(e.message);
+          }
+        })();
+      },
+    }
+  }
 </script>
 
 <style scoped>
